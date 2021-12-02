6bf98c65e09963594575dcfdaf49d3d5b95ddd3c
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-12-02 15:20:42 +0100

    start

---
+++ classes/Movie.php
@@ -0,0 +1,8 @@
+<?php
+class Movie
+{
+    public $title = "N/D";
+    public $director = "N/D";
+    public $year = "N/D";
+    public $category = "N/D";
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . "/classes/Movie.php";
+
+$movie1 = new Movie();
+$movie1->title = "Harry Potter";
+
+var_dump($movie1);
