c460cdf3bafafd469a7fceaa9985ba221ed22d64
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-12-02 15:48:10 +0100

    getter e setter

--- classes/Movie.php
+++ classes/Movie.php
@@ -4,5 +4,61 @@ class Movie
     public $title = "N/D";
     public $director = "N/D";
     public $year = "N/D";
+    public $duration = "N/D";
     public $category = "N/D";
+
+    /* GETTER E SETTER TITLE */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($newTitle)
+    {
+        $this->title = $newTitle;
+    }
+
+    /* GETTER E SETTER DIRECTOR */
+    public function getDirector()
+    {
+        return $this->director;
+    }
+
+    public function setDirector($newDirector)
+    {
+        $this->director = $newDirector;
+    }
+
+    /* GETTER E SETTER YEAR */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    public function setYear($newYear)
+    {
+        $this->year = $newYear;
+    }
+
+    /* GETTER E SETTER DURATION */
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    public function setDuration($newDuration)
+    {
+        $this->duration = $newDuration;
+    }
+
+    /* GETTER E SETTER CATEGORY */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function setCategory($newCategory)
+    {
+        $this->category = $newCategory;
+    }
 }
--- index.php
+++ index.php
@@ -2,6 +2,19 @@
 require_once __DIR__ . "/classes/Movie.php";
 
 $movie1 = new Movie();
-$movie1->title = "Harry Potter";
+$movie1->setTitle("Harry Potter e la pietra filosofale");
+$movie1->setDirector("Chris Columbus");
+$movie1->setYear("2001");
+$movie1->setDuration("152 min");
+$movie1->setCategory("Fantasy");
 
 var_dump($movie1);
+
+$movie2 = new Movie();
+$movie2->setTitle("Il Signore degli Anelli - La Compagnia dell'Anello");
+$movie2->setDirector("Peter Jackson");
+$movie2->setYear("2001");
+$movie2->setDuration("178 min");
+$movie2->setCategory("Fantasy");
+
+var_dump($movie2);
